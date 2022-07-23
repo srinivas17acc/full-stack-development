793af87f4ac57881b9ed5e9c54befe94fc59ced8
srinivas17acc <srinivas17acc@gmail.com>
2022-07-23 23:01:38 +0530

    express event bus initial commit and communication happened from all the services

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 
 const app = express();
@@ -15,14 +16,28 @@ app.get('/posts/:id/comments', (req, res) => {
 });
 
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async(req, res) => {
      const commentId = randomBytes(4).toString('hex');
      const {content} = req.body; 
      const comments = commentsByPostId[req.params.id] || [];
      comments.push({id: commentId, content});
      commentsByPostId[req.params.id] = comments;
-     console.log(commentsByPostId, 'logs');
+
+     await axios.post('http://localhost:4005/events',{
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId : req.params.id
+        }
+     }).catch(err => console.log(err, 'testing '))
      res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received events :', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, ()=> {
