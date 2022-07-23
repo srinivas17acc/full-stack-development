9c34c7cb4d13a97958bb8bbce1fba8f43a1046bd
srinivas17acc <srinivas17acc@gmail.com>
2022-07-24 01:19:55 +0530

    query service impl and communicated with all services

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default ({ postId })=> {
-     const [comments, setComments] = useState([]);
-
-     const fetchData = async ()=> {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-
-        setComments(res.data);
-     };
-
-     useEffect(() => {
-        fetchData();
-     },[]);
-
+export default ({ comments })=> {
      const renderedComments = comments.map(Comment => {
         return <li key={Comment.id}>{Comment.content}</li>;
      });
