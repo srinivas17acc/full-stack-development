c64703290c99a66669a71def170ca585e47f0a40
srinivas17acc <srinivas17acc@gmail.com>
2022-07-25 03:02:02 +0530

    moderation service and Mini microservice app demo demo

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,8 +2,24 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default ({ comments })=> {
-     const renderedComments = comments.map(Comment => {
-        return <li key={Comment.id}>{Comment.content}</li>;
+  
+     const renderedComments = comments.map(comment => {
+
+      let content;
+   
+      if (comment.status === 'approved') {
+        content = comment.content;
+      }
+ 
+      if (comment.status === 'pending') {
+        content = 'waiting for Moderation'
+      }
+ 
+      if (comment.status === 'rejected') {
+        content = 'commend has been rejected'
+      }
+ 
+        return <li key={Comment.id}>{content}</li>;
      });
 
     return <ul>{renderedComments}</ul>;
--- query/index.js
+++ query/index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+
+const handleEvent = (type, data) => {
+
+    if (type ==='PostCreated') {
+        const {id, title} = data;
+        posts[id] = {id, title, comments:[]};
+    }
+    if (type ==='CommentCreated') {
+        const {id, content, postId, status} = data;
+        const post = posts[postId];
+        post.comments.push({id, content, status});
+    }
+
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
 
 app.get('/posts',(req, res) => {
     res.send(posts);
@@ -15,20 +41,22 @@ app.get('/posts',(req, res) => {
 
 app.post('/events',(req, res) => {
     const {type, data} = req.body;
-
-    if (type ==='PostCreated') {
-        const {id, title} = data;
-        posts[id] = {id, title, comments:[]};
-    }
-    if (type ==='CommentCreated') {
-        const {id, content, postId} = data;
-        const post = posts[postId];
-        post.comments.push({id, content});
-    }
-    console.log(posts);
+    handleEvent(type, data);
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listing port 4002');
+
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+        for (let event of res.data) {
+            console.log('processing event : ', event.type);
+            handleEvent(event.type, event.data);
+        }
+        
+    }catch(error) {
+        console.log(error.message);
+    }
 });
